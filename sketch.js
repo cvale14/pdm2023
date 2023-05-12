5ee6e90dd4e94a574a7a143cef16d7dcf1fa6a57
cvale14 <98445249+cvale14@users.noreply.github.com>
2023-05-11 19:38:55 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,107 @@
+var Button;
+var currentColor;
+
+
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1600, 2500);
+  background(255);
+  noStroke()
 }
 
+
 function draw() {
-  background(0, 0, 180);
-  strokeWeight(6); // Thicker
-  stroke(255,255,255)
-  fill(0, 175, 0)
-  circle(290, 250, 300);
- 
-  fill(225, 0, 0)
-    push();
-    translate(width * 0.8, height * 0.5);
-    star(-180, -40, 160, 60, 5);
-    pop();
-    function star(x, y, radius1, radius2, npoints) {
-      let angle = TWO_PI / npoints;
-      let halfAngle = angle / 2.0;
-      beginShape();
-      for (let a = 0; a < TWO_PI; a += angle) {
-        let sx = x + cos(a) * radius2;
-        let sy = y + sin(a) * radius2;
-        vertex(sx, sy);
-        sx = x + cos(a + halfAngle) * radius1;
-        sy = y + sin(a + halfAngle) * radius1;
-        vertex(sx, sy);
-      }
-      endShape(CLOSE);
-    }
-    
-  }
+  //background(255);
+    let c = (0, 0, 0);
   
+  noStroke();
+  fill(255,0,0);
+  square(3,10,30,0);
+  
+  fill(255,160,0);
+  square(3,42,30,0);
+  
+  fill(255,255,0);
+  square(3,74,30,0);
+  
+  fill(0,255,0);
+  square(3,106,30,0);
+  
+  fill(0,255,255);
+  square(3,137,30,0);
+  
+  fill(255,100,200);
+  square(3,168,30,0);
+  
+  fill(200,100,50);
+  square(3,200,30,0);
+  
+  fill(255,255,255);
+  square(3,233,30,0);
+  
+  fill(0,0,255);
+  square(3,266,30,0);
+  
+  fill(0,0,0);
+  square(3,300,30,0);
+
+   
+  
+  
+}
+ 
+function mousePressed(){
+  if(mouseX >= 3 && mouseX <= 33){
+
+    if (mouseY >=10 && mouseY <=30){
+      currentColor = color(255,0,0);
+    }
+    else if (mouseY >=42 && mouseY <=72){
+      currentColor = color(255,160,0);
+      
+
+    }
+    else if(mouseY >=74 && mouseY <=104){
+      currentColor = color(255,255,0);
+    }
+
+    else if(mouseY >=106  && mouseY <=136){
+      currentColor = color(0,255,0);
+    }
+
+    else if(mouseY >=137  && mouseY <=167){
+      currentColor = color(0,255,255);
+    }
+
+    else if(mouseY >=168  && mouseY <=198){
+      currentColor = color(255,100,200);
+    }
+
+    else if(mouseY >=200  && mouseY <=230){
+      currentColor = color(200,100,50);
+    }
+
+    else if(mouseY >=233  && mouseY <=263){
+      currentColor = color(255,255,255);
+    }
+
+    else if(mouseY >=266  && mouseY <=296){
+      currentColor = color(0,0,255);
+    }
+
+    else if(mouseY >=300  && mouseY <=330){
+      currentColor = color(0,0,0);
+    }
+
+
+  }
+}
+
+function mouseDragged() {
+  if (mouseIsPressed) {
+    noStroke();
+   fill(currentColor);
+   ellipse(mouseX, mouseY,80,80);
+  
+ }
+}
  
